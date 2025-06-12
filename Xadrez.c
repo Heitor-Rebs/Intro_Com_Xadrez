f2928b3aeede0028c3f891f5e96dc3971f4e5aaa
Heitor-Rebs <heitor.rebordoes@gmail.com>
2025-06-11 22:47:48 -0300

    Update Xadrez.c

--- Xadrez.c
+++ Xadrez.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
 
+/* Criando as funções de recursividade para a torre, bispo e rainha */
+void torre(int i){
+    if  (i>0){
+        printf("Direita\n");
+        torre(i-1);
+    }
+}
+
+void bispo(int i){
+    if  (i>0){
+        printf("Cima, Direita\n");
+        bispo(i-1);
+    }
+}
+
+void rainha(int i){
+    if  (i>0){
+        printf("Esquerda\n");
+        rainha(i-1);
+    }
+}
+
 int main(){
     /* Declarando variáveis */
     int i = 0, j = 0;
 
+
+
     /* Mostrando movimento da torre */
     printf("Movimentos da torre\n");
-    for(i=0;i<5;i++){
-        printf("Direita\n");
-    }
+    torre(5);
 
     /* Zerando variável i, para reutilizar no loop */
     i = 0;
 
     /* Mostrando movimento do bispo */
     printf("\nMovimentos do bispo\n");
-    while(i<5){
-        printf("Cima, Direita\n");
-        i++;
-    }
+    bispo(5);
 
     i = 0;
 
     /* Mostrando movimento da rainha */
     printf("\nMovimentos da rainha\n");
-    do{
-        printf("Esquerda\n");
-        i++;
-    }while(i<8);
+    rainha(8);
 
     i = 0;
 
@@ -36,9 +52,26 @@ int main(){
     printf("Movimentos do cavalo\n");
     do{
         for(j=0;j<2;j++){
-            printf("Baixo\n");
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n");
         i++;
     }while(i<1);
+
+    i = 0;
+    j = 0;
+
+    /* Mostrando movimento do bispo com loop aninhado */
+    printf("\n------------------------\n");
+    printf("Movimentos do bispo\n");
+    for(i=0; i<5;i++){
+        printf("Cima\n");
+        for(j=0;j<=5;j++){
+            /* Usando if, para apenas mostrar o movimento para direita uma vez para cada movimento para baixo */
+            if(i==j){
+                printf("Direita\n");
+            }
+        }
+    }
+
 }
